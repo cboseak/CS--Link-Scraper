e85ac9a51c291437a229001fb284f9d4ddf9c545
Christopher Boseak <cboseak@Gmail.com>
2015-12-28 21:10:28 -0600

    Cleaned up code and added comments

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -28,14 +28,24 @@ namespace WindowsFormsApplication1
         List<string> linkQueue = new List<string>();
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
+            //when each page loads, make sure the address bar has an accurate address
             textBox3.Text = webBrowser1.Url.ToString();
+            
+            //MANUAL MODE - This hit if you're not in autoscrape mode and you browse. 
+            //Every page visited pull all links and adds them to the lists
             if(!autoScrape && !firstTime && manualMode) linkListMaker();
+            
+            //Autoscape is AND there are less links than requested AND it is the first time around
+            //then pull all links from current page, set first time to false, and update the link count
             if(autoScrape && links.Count <= numericUpDown1.Value && firstTimeAuto)
             {
                 linkListMaker();
                 firstTimeAuto = false;
                 updateLinkCount();
             }
+            
+            //Autoscape is AND there are less links than requested AND it is at least second time around
+            //then browse to the link at position zero, pull the links on that page, at remove link at position zero
             if(autoScrape && links.Count <= numericUpDown1.Value && !firstTimeAuto)
             {
                 if (linkQueue.ElementAt(0) != null && !webBrowser1.IsBusy)
@@ -54,6 +64,7 @@ namespace WindowsFormsApplication1
         {
             HtmlElementCollection link = webBrowser1.Document.GetElementsByTagName("A");
 
+            //for each link collected, add to lists
             foreach (HtmlElement i in link)
             {
                 links.Add(i.GetAttribute("href").ToString());
@@ -63,6 +74,7 @@ namespace WindowsFormsApplication1
             linkQueue = removeDuplicates(linkQueue);
             textBox1.Clear();
        
+            //for each link in links list, output to text box
             foreach (var i in links)
             {
                 if (i.Contains("http://") || i.Contains("https://"))
@@ -73,6 +85,7 @@ namespace WindowsFormsApplication1
             if (links.Count >= numericUpDown1.Value) autoScrape = false;
         }
 
+        //GO BUTTON on Manual Page    
         private void goButton_Click(object sender, EventArgs e)
         {
             var tempUrl = "";
@@ -83,12 +96,14 @@ namespace WindowsFormsApplication1
             webBrowser1.Navigate(new Uri(tempUrl));
             manualMode = true;
         }
-
+        
+        //BACK BUTTON on Manual Page
         private void backButton_Click(object sender, EventArgs e)
         {
             webBrowser1.GoBack();
         }
 
+        //FORWARD BUTTON on Manual Page
         private void forwardButton_Click(object sender, EventArgs e)
         {
             webBrowser1.GoForward();
@@ -103,12 +118,14 @@ namespace WindowsFormsApplication1
             }
         }
 
+        //Removes duplicates in list that is fed in.
         private List<string> removeDuplicates(List<string> listIn)
         {
             listIn = listIn.Distinct().ToList();
             return listIn;
         }
 
+        //GO BUTTON on Auto Page 
         private void button1_Click(object sender, EventArgs e)
         {
             if(links.Count >= numericUpDown1.Value)
@@ -134,16 +151,19 @@ namespace WindowsFormsApplication1
             }
         }
 
+        //sets automatic scrape status to false so loop will stop
         private void button2_Click(object sender, EventArgs e)
         {
             autoScrape = false;
         }
 
+        //CLEAR BUTTON - Call clear all function below
         private void button3_Click(object sender, EventArgs e)
         {
             clearAll();
         }
 
+        //checks if running so that it can update the status label
         private void checkIfRunning()
         {
             if(autoScrape)
@@ -158,10 +178,13 @@ namespace WindowsFormsApplication1
             }
         }
 
+        //updates label on UI to show the current amount on links scraped
         private void updateLinkCount()
         {
             linkCount.Text = links.Count.ToString();
         }
+        
+        //clear all text boxes and lists
         private void clearAll()
         {
             autoScrape = false;
@@ -173,7 +196,8 @@ namespace WindowsFormsApplication1
             updateLinkCount();
             checkIfRunning();
         }
-
+        
+        //if enter is hit in the address bar, click go
         private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
         {
             if(e.KeyChar == (char)Keys.Enter)
